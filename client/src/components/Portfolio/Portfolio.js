51b1f872b6e9e35214b41061b68c7b773a9ea83c
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-01-15 17:16:03 -0500

    Jumbotron styling and remove redundant files.

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -4,6 +4,7 @@ import projectStore from '../../utils/projectStore'
 import './Portfolio.css'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
+import Typical from 'react-typical'
 
 const Portfolio = () => {
   const responsive = {
@@ -27,11 +28,60 @@ const Portfolio = () => {
   }
 
   return (
-    <Carousel responsive={responsive}>
-      {projectStore.map((project) => (
-        <Project project={project} />
-      ))}
-    </Carousel>
+    <>
+      <h1 id="myProjects">
+        <Typical steps={['My Projects', 1500]} loop={Infinity} wrapper="b" />
+      </h1>
+      <div className="dropdown-divider"></div>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 1,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 1,
+          },
+        }}
+        showDots
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+        {projectStore.map((project) => (
+          <Project project={project} />
+        ))}
+      </Carousel>
+    </>
   )
 }
 
--- client/src/components/Portfolio/Project/Project.js
+++ client/src/components/Portfolio/Project/Project.js
@@ -4,6 +4,7 @@ import './Project.css'
 const Project = ({
   project: { title, img, alt, description, link, github },
 }) => {
+  // style={{ backgroundImage: `url(${img}`, backgroundSize: 'cover' }}
   return (
     <div class="jumbotron">
       <div class="container">
@@ -37,29 +38,3 @@ const Project = ({
 }
 
 export default Project
-
-{
-  /* <div className="card expand my-3">
-      <img src={img} className="card-img-top" alt={alt} />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <a
-          href={link}
-          target="_blank"
-          className="btn btn-primary"
-          rel="noreferrer"
-        >
-          Link to application <i className="fa fa-chevron-right"></i>
-        </a>
-        <a
-          href={github}
-          target="_blank"
-          className="btn btn-primary"
-          rel="noreferrer"
-        >
-          See on GitHub <i className="fa fa-chevron-right"></i>
-        </a>{' '}
-      </div>
-    </div> */
-}
