0bdcc19ba3d4c56a41e7152709e9e0440abf2056
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-04 22:14:53 -0500

    Finish basic card styling version 2

--- client/src/components/Portfolio/ProjectV2/ProjectV2.js
+++ client/src/components/Portfolio/ProjectV2/ProjectV2.js
@@ -5,20 +5,35 @@ const ProjectV2 = ({
   project: { title, img, alt, description, link, github },
 }) => {
   return (
-    <div className="container">
+    <div className="cardContainer">
       <div className="projectCard">
         <div className="image">
-          <div className="backgroundImage"></div>
-          <img src="https://placehold.it/300x300" alt={alt} />
+          <img
+            className="appImage"
+            src="https://placehold.it/250x250"
+            alt={alt}
+          />
         </div>
-        <h3>{title}</h3>
+        <h3 className="title">{title}</h3>
         <p className="description">{description}</p>
-        <a className="githubBtn" href={github} target="_blank" rel="noreferrer">
-          <button>GitHub</button>
-        </a>
-        <a className="deployedBtn" href={link} target="_blank" rel="noreferrer">
-          <button>Demo</button>
-        </a>
+        <div className="links">
+          <a
+            className="githubBtn"
+            href={github}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button>GitHub</button>
+          </a>
+          <a
+            className="deployedBtn"
+            href={link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button>Demo</button>
+          </a>
+        </div>
       </div>
     </div>
   )
