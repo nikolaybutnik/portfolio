19f8f3a09696b2e3d1c78342cadcb94960ca798a
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-08 21:10:26 -0500

    Override bootstrap styles in navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import './NavBar.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+// import NavDropdown from 'react-bootstrap/NavDropdown'
+// import Form from 'react-bootstrap/Form'
+// import FormControl from 'react-bootstrap/FormControl'
+// import Button from 'react-bootstrap/Button'
+import './NavBar.css'
 
 const NavBar = () => {
-  const style = {
-    backgroundColor: 'red',
-  }
   return (
-    <Navbar className="navbar" bg="light" expand="md" style={style}>
+    <Navbar expand="md">
       {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/portfolio">Projects</Nav.Link>
+          <Nav.Link className="hvr-underline-from-left" href="/home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="hvr-underline-from-left" href="/portfolio">
+            Projects
+          </Nav.Link>
           {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
