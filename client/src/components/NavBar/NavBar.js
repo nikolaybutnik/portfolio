7e4d86e65cecd67e2978997bdc34ab6ab6c7ae29
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-10 21:31:23 -0500

    Make the navbar sticky on scroll

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,57 +1,77 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import './NavBar.css'
 
 const NavBar = () => {
+  const navRef = useRef()
+
+  useEffect(() => {
+    const sticky = navRef.current.offsetTop
+    window.onscroll = () => {
+      stickyNav()
+    }
+    const stickyNav = () => {
+      console.log(sticky)
+      console.log(window.pageYOffset)
+      if (window.pageYOffset > sticky) {
+        navRef.current.classList.add('sticky')
+      } else {
+        navRef.current.classList.remove('sticky')
+      }
+    }
+  })
+
   return (
-    <Navbar expand="md">
-      {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link className="hvr-underline-from-left" href="/home">
-            Home
-          </Nav.Link>
-          <Nav.Link className="hvr-underline-from-left" href="/portfolio">
-            Projects
-          </Nav.Link>
-        </Nav>
-        <a
-          href="https://www.linkedin.com/in/nikolay-butnik/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="navIcon"
-            src="assets/images/linkedinIconNav.png"
-            alt="Twitter"
-          />
-        </a>
-        <a
-          href="https://github.com/nikolaybutnik"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="navIcon"
-            src="assets/images/githubIconNav.png"
-            alt="GitHub"
-          />
-        </a>
-        <a
-          href="https://twitter.com/nikolaybutnik"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="navIcon"
-            src="assets/images/twitterIconNav.png"
-            alt="Twitter"
-          />
-        </a>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav ref={navRef}>
+      <Navbar expand="md">
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link className="hvr-underline-from-left" href="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="hvr-underline-from-left" href="/portfolio">
+              Projects
+            </Nav.Link>
+          </Nav>
+          <a
+            href="https://www.linkedin.com/in/nikolay-butnik/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="navIcon"
+              src="assets/images/linkedinIconNav.png"
+              alt="Twitter"
+            />
+          </a>
+          <a
+            href="https://github.com/nikolaybutnik"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="navIcon"
+              src="assets/images/githubIconNav.png"
+              alt="GitHub"
+            />
+          </a>
+          <a
+            href="https://twitter.com/nikolaybutnik"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="navIcon"
+              src="assets/images/twitterIconNav.png"
+              alt="Twitter"
+            />
+          </a>
+        </Navbar.Collapse>
+      </Navbar>
+    </nav>
   )
 }
 
