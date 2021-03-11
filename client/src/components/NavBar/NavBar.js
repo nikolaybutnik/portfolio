7a4e01d31ac90237be6b0a846465ee830aaf6054
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-10 21:01:46 -0500

    Remove unused navbar code

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
-// import Form from 'react-bootstrap/Form'
-// import FormControl from 'react-bootstrap/FormControl'
-// import Button from 'react-bootstrap/Button'
 import './NavBar.css'
 
 const NavBar = () => {
@@ -20,17 +16,6 @@ const NavBar = () => {
           <Nav.Link className="hvr-underline-from-left" href="/portfolio">
             Projects
           </Nav.Link>
-          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown> */}
         </Nav>
         <a
           href="https://www.linkedin.com/in/nikolay-butnik/"
@@ -65,11 +50,6 @@ const NavBar = () => {
             alt="Twitter"
           />
         </a>
-
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
       </Navbar.Collapse>
     </Navbar>
   )
