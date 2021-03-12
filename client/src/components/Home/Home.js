932cce734a45ceaa424bc21b35ba3b0097bdbdc3
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-11 20:52:13 -0500

    Refactor landing page and associated scss

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -13,8 +13,9 @@ const Main = () => {
           wrapper="b"
         />
       </h1>
-      <hr className="solidDivider"></hr>
-      <div className="homeContent">
+
+      <div className="landingPage">
+        <hr className="solidDivider"></hr>
         <div className="backgroundContainer">
           <img
             className="hvr-glow"
@@ -23,86 +24,52 @@ const Main = () => {
             alt="Profile headshot"
           />
         </div>
-      </div>
-      <hr className="solidDivider"></hr>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <h1 className="text-center" id="greeting">
-              // Hello world, I'm Nik
-            </h1>
-            <h2 className="text-center" id="tags">
-              // I'm a
-              <Typical
-                steps={[
-                  ' full stack web developer',
-                  1500,
-                  ' tech enthusiast',
-                  1500,
-                  ' lifelong learner',
-                  1500,
-                  ' creative problem solver',
-                  1500,
-                  'n unshakable optimist',
-                  1500,
-                ]}
-                loop={Infinity}
-                wrapper="b"
-              />
-            </h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <div className="d-flex justify-content-center">
-              <a
-                href="https://www.linkedin.com/in/nikolay-butnik/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="icon d-block"
-                  src="assets/images/linkedinIcon.png"
-                  alt="linkedin link"
-                />
-              </a>
-              <a
-                href="https://github.com/nikolaybutnik"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="icon d-block"
-                  src="assets/images/githubIcon.png"
-                  alt="github link"
-                />
-              </a>
+        <hr className="solidDivider"></hr>
 
-              <a href="mailto: btnk.nik@gmail.com">
-                <img
-                  className="icon d-block"
-                  src="assets/images/emailIcon.png"
-                  alt="email link"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-5">
-          <div className="col text-center">
-            <a
-              id="resume"
-              href="https://drive.google.com/file/d/1AtypBF0fYdustOYBHz9Gel43MEyMPelN/view?usp=sharing"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Typical
-                steps={['getResume()', 1500]}
-                loop={Infinity}
-                wrapper="b"
-              />
-            </a>
-          </div>
+        <h1 className="text-center" id="greeting">
+          // Hello world, I'm Nik
+        </h1>
+
+        <h2 className="text-center" id="dynamicTyping">
+          // I'm a
+          <Typical
+            steps={[
+              ' full stack web developer',
+              1500,
+              ' tech enthusiast',
+              1500,
+              ' lifelong learner',
+              1500,
+              ' creative problem solver',
+              1500,
+              'n unshakable optimist',
+              1500,
+            ]}
+            loop={Infinity}
+            wrapper="b"
+          />
+        </h2>
+
+        <div className="landingPageContent">
+          <a
+            id="resume"
+            href="https://drive.google.com/file/d/1AtypBF0fYdustOYBHz9Gel43MEyMPelN/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Typical
+              steps={['getResume()', 1500]}
+              loop={Infinity}
+              wrapper="b"
+            />
+          </a>
+          <a id="projects" href="/portfolio" target="_blank" rel="noreferrer">
+            <Typical
+              steps={['getProjects()', 1500]}
+              loop={Infinity}
+              wrapper="b"
+            />
+          </a>
         </div>
       </div>
     </>
