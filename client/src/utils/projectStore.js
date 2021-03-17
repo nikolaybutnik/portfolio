9f6a64cf5abe971597e3fb15e011731d33994b5a
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-03-16 22:03:14 -0400

    Use new compressed images for better performance

--- client/src/utils/projectStore.js
+++ client/src/utils/projectStore.js
@@ -2,8 +2,8 @@ const projectStore = [
   {
     title: 'STAR ACADEMY',
     img: 'assets/images/star-academy-250x250.png',
-    imgDesktop: 'assets/images/StarAcademyDesktop.png',
-    imgMobile: 'assets/images/StarAcademyMobile.png',
+    imgDesktop: 'assets/images/StarAcademyDesktop-min.png',
+    imgMobile: 'assets/images/StarAcademyMobile-min.png',
     alt: 'Star Academy',
     description: 'Learn music the fun way with daily theory quizzes.',
     link: 'https://guarded-crag-12899.herokuapp.com/',
@@ -12,8 +12,8 @@ const projectStore = [
   {
     title: 'OOP GENERAL HOSPITAL',
     img: 'assets/images/oop-general-hospital-250x250.png',
-    imgDesktop: 'assets/images/OOPDesktop.png',
-    imgMobile: 'assets/images/OOPMobile.png',
+    imgDesktop: 'assets/images/OOPDesktop-min.png',
+    imgMobile: 'assets/images/OOPMobile-min.png',
     alt: 'OOP Genral Hospital',
     description:
       'A web application that utilizes databases to simulate a humorous hospital scenario.',
@@ -23,8 +23,8 @@ const projectStore = [
   {
     title: 'STOCKBUDDY',
     img: 'assets/images/stockbuddy-250x250.png',
-    imgDesktop: 'assets/images/StockbuddyDesktop.png',
-    imgMobile: 'assets/images/StockbuddyMobile.png',
+    imgDesktop: 'assets/images/StockbuddyDesktop-min.png',
+    imgMobile: 'assets/images/StockbuddyMobile-min.png',
     alt: 'Stockbuddy',
     description:
       'Stockbuddy is a web application that helps beginners enter the world of stock trading.',
@@ -34,8 +34,8 @@ const projectStore = [
   {
     title: 'GOOGLE BOOKS SEARCH',
     img: 'assets/images/google-books-search-250x250.png',
-    imgDesktop: 'assets/images/GoogleBooksDesktop.png',
-    imgMobile: 'assets/images/GoogleBooksMobile.png',
+    imgDesktop: 'assets/images/GoogleBooksDesktop-min.png',
+    imgMobile: 'assets/images/GoogleBooksMobile-min.png',
     alt: 'Google Books Search',
     description:
       'A web application that allows the user the search for books and save their favorites.',
@@ -45,8 +45,8 @@ const projectStore = [
   {
     title: 'WEATHER DASHBOARD',
     img: 'assets/images/weather-dashboard-250x250.png',
-    imgDesktop: 'assets/images/WeatherDesktop.png',
-    imgMobile: 'assets/images/WeatherMobile.png',
+    imgDesktop: 'assets/images/WeatherDesktop-min.png',
+    imgMobile: 'assets/images/WeatherMobile-min.png',
     alt: 'Weather Dashboard',
     description:
       'Search for immediate and future weather conditions around the world.',
@@ -56,8 +56,8 @@ const projectStore = [
   {
     title: 'WORK DAY SCHEDULER',
     img: 'assets/images/scheduler-250x250.png',
-    imgDesktop: 'assets/images/SchedulerDesktop.png',
-    imgMobile: 'assets/images/SchedulerMobile.png',
+    imgDesktop: 'assets/images/SchedulerDesktop-min.png',
+    imgMobile: 'assets/images/SchedulerMobile-min.png',
     alt: 'Work Day Scheduler',
     description: 'A simple calendar tool to organize the current work day.',
     link: 'https://nikolaybutnik.github.io/work-day-scheduler/',
@@ -66,8 +66,8 @@ const projectStore = [
   {
     title: 'EAT-DA-BURGER!',
     img: 'assets/images/burger-250x250.png',
-    imgDesktop: 'assets/images/BurgerDesktop.png',
-    imgMobile: 'assets/images/BurgerMobile.png',
+    imgDesktop: 'assets/images/BurgerDesktop-min.png',
+    imgMobile: 'assets/images/BurgerMobile-min.png',
     alt: 'Eat-Da-Burger',
     description:
       'A web application that keeps track of the burgers you want to eat, and those you have eaten!',
@@ -77,8 +77,8 @@ const projectStore = [
   {
     title: 'EMPLOYEE DIRECTORY',
     img: 'assets/images/employee-directory-250x250.png',
-    imgDesktop: 'assets/images/EmployeesDesktop.png',
-    imgMobile: 'assets/images/EmployeesMobile.png',
+    imgDesktop: 'assets/images/EmployeesDesktop-min.png',
+    imgMobile: 'assets/images/EmployeesMobile-min.png',
     alt: 'Employee Directory',
     description:
       'A web application that allows the user to view, filter and sort a list of employees.',
